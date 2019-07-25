f798f33369b0890eabddca027b28533b6237be41
Michael Fladischer <michael@fladi.at>
2019-07-25 10:55:43 +0200

    Add periodic task to clean up orphaned users.

--- src/outpost/django/salt/tasks.py
+++ src/outpost/django/salt/tasks.py
@@ -7,8 +7,21 @@ from celery.task import PeriodicTask, Task
 from django.core.cache import cache
 
 from .conf import settings
+from .models import User, Person, Student
 
 logger = logging.getLogger(__name__)
+
+
+class CleanUsersTask(PeriodicTask):
+    run_every = timedelta(minutes=5)
+
+    def run(self, **kwargs):
+        for u in User.objects.all():
+            try:
+                u.person
+            except (Person.DoesNotExist, Student.DoesNotExist) as e:
+                logger.warn(f"Removing {u._meta.label} with PK {u.pk} because of {e}")
+                u.delete()
 
 
 class RefreshPasswordTask(PeriodicTask):
