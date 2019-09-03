48da7a993b0101f8c5e597b1cf674a835c7d02b6
Michael Fladischer <michael@fladi.at>
2019-09-03 14:12:57 +0200

    Add Salt API command executor task.

--- src/outpost/django/salt/tasks.py
+++ src/outpost/django/salt/tasks.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 import requests
 from celery.task import PeriodicTask, Task
+from purl import URL
 
 from .conf import settings
 from .models import SystemUser, User
@@ -21,5 +22,28 @@ class CleanUsersTask(PeriodicTask):
                 logger.warn(f"Removing {u._meta.label} with PK {u.pk} because of {e}")
                 u.delete()
 
+
 class RunCommandTask(Task):
-    pass
+    def __init__(self, *args, **kwargs):
+        self.session = requests.Session()
+        self.session.headers.update({"Accept": "application/json"})
+
+    def run(self, **kwargs):
+        url = URL(settings.SALT_MANAGEMENT_URL)
+        kwargs.update(
+            {
+                "username": settings.SALT_MANAGEMENT_USER,
+                "password": settings.SALT_MANAGEMENT_PASSWORD,
+                "eauth": "rest",
+                "client": "local",
+            }
+        )
+        try:
+            result = self.session.post(
+                url.add_path_segment("run").as_string(), data=kwargs
+            )
+            result.raise_for_status()
+        except requests.RequestException as e:
+            logger.error(f"Failed to run task through Salt API: {e}")
+        logger.debug(f"Scheduled job through Salt API: {result.text}")
+        return result.json()
