5d5b3282e24dd82f091373dea900537683a2d2f3
Michael Fladischer <michael@fladi.at>
2019-09-03 14:12:11 +0200

    Use SystemUser objects to check for existing persons.

--- src/outpost/django/salt/tasks.py
+++ src/outpost/django/salt/tasks.py
@@ -5,7 +5,7 @@ import requests
 from celery.task import PeriodicTask, Task
 
 from .conf import settings
-from .models import User, Person, Student
+from .models import SystemUser, User
 
 logger = logging.getLogger(__name__)
 
@@ -14,10 +14,10 @@ class CleanUsersTask(PeriodicTask):
     run_every = timedelta(minutes=5)
 
     def run(self, **kwargs):
-        for u in User.objects.all():
+        for u in SystemUser.objects.all():
             try:
-                u.person
-            except (Person.DoesNotExist, Student.DoesNotExist) as e:
+                str(u.user.person)
+            except (User.DoesNotExist) as e:
                 logger.warn(f"Removing {u._meta.label} with PK {u.pk} because of {e}")
                 u.delete()
 
