79a77c09fb137f4249e8d65fdb80730471967c2b
Michael Fladischer <michael@fladi.at>
2020-08-04 22:15:22 +0200

    Reformat using black.

--- src/outpost/django/salt/migrations/0002_user_active.py
+++ src/outpost/django/salt/migrations/0002_user_active.py
@@ -7,12 +7,10 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ("salt", "0001_initial"),
-    ]
+    dependencies = [("salt", "0001_initial")]
 
     operations = [
         migrations.AddField(
-            model_name="user", name="active", field=models.BooleanField(default=True),
-        ),
+            model_name="user", name="active", field=models.BooleanField(default=True)
+        )
     ]
